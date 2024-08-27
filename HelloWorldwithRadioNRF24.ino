1811a6280dcb81c62d41699f13ec4b74710f8578
JasprtapGoomer <112976232+JasprtapGoomer@users.noreply.github.com>
2024-08-27 18:54:49 -0400

    Update HelloWorldwithRadioNRF24.ino

--- HelloWorldwithRadioNRF24.ino
+++ HelloWorldwithRadioNRF24.ino
@@ -1,42 +1,53 @@
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8);  // CE, CSN
-RF24 radio1(9, 10); // CE, CSN
+// CE and CSN pin assignments
+RF24 radio(7, 8); // CE, CSN pins
 
 const byte address[6] = "00001";
+int joy1X, joy1Y, joy2X, joy2Y;
 
 void setup() {
-  Serial.begin(9600);  // Initialize serial communication
+  // Set up Serial Monitor for debugging
+  Serial.begin(9600);
   
-  // First radio as Transmitter
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
-  
-  // Second radio as receiver
-  radio1.begin();
-  radio1.openReadingPipe(0, address);
-  radio1.setPALevel(RF24_PA_MIN);
-  radio1.startListening();
+  // Set SPI pins
+  pinMode(9, INPUT);   // MISO as input
+  pinMode(10, OUTPUT); // MOSI as output
+  pinMode(11, OUTPUT); // SCK as output
+
+  SPI.begin(); // Initialize SPI communication
+  radio.begin(); // Initialize nRF24L01
+  radio.openWritingPipe(address); // Open writing pipe with a specific address
+  radio.setPALevel(RF24_PA_MIN); // Set power level
+  radio.stopListening(); // Set nRF24L01 as transmitter
 }
 
 void loop() {
-  const char text[] = "Hello World";
+  // Read joystick positions
+  joy1X = analogRead(A0); // Joystick 1 X-axis
+  joy1Y = analogRead(A1); // Joystick 1 Y-axis
+  joy2X = analogRead(A3); // Joystick 2 X-axis
+  joy2Y = analogRead(A4); // Joystick 2 Y-axis
   
-  // Transmit with first radio
-  radio.write(&text, sizeof(text));
-  
+  // Create an array with joystick data
+  int joystickData[4] = {joy1X, joy1Y, joy2X, joy2Y};
 
-  
-  // If data has been received, printout received text
-  if (radio1.available()) {
-    char receivedText[32] = "";
-    radio1.read(&receivedText, sizeof(receivedText));
-    Serial.println(receivedText);
+  // Print the joystick coordinates to Serial Monitor
+  Serial.print("Joystick 1 - X: "); Serial.print(joy1X);
+  Serial.print(" Y: "); Serial.println(joy1Y);
+  Serial.print("Joystick 2 - X: "); Serial.print(joy2X);
+  Serial.print(" Y: "); Serial.println(joy2Y);
+
+  // Print the data being sent
+  Serial.print("Sending: ");
+  for(int i = 0; i < 4; i++) {
+    Serial.print(joystickData[i]);
+    if(i < 3) Serial.print(", ");
   }
-  
-  delay(1000);
+  Serial.println();
+
+  // Send joystick data over nRF24L01
+  radio.write(&joystickData, sizeof(joystickData));
+  delay(100);
 }
